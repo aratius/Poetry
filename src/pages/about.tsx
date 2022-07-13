f02d2512373e3f98e7c086ab07cb863fdd71e90d
aratius <arata1129matsu@icloud.com>
2022-07-13 16:10:11 +0900

    aboutページ

---
+++ src/pages/about.tsx
@@ -0,0 +1,45 @@
+import { Component, ReactElement } from "react";
+import MicroCMS from "src/cms/microCms";
+import Header from "src/components/common/header";
+import Works from "src/components/works";
+import { API } from "src/constants/api";
+import { _About, _Works } from "src/types/cms";
+
+interface Props {
+	about: _About
+}
+interface State { }
+
+export default class About extends Component<Props, State> {
+
+	public state: State = {};
+	constructor(props: Props) {
+		super(props);
+		this.state = {};
+	}
+
+	public componentDidMount(): void {
+	}
+
+	public render(): ReactElement {
+		const {about} = this.props
+		return (
+			<>
+				<Header name={about.name}/>
+				<div className="about__wrapper">
+					<h1>Hello, my name is {about.name}.</h1>
+				</div>
+			</>
+		);
+	}
+}
+
+export const getStaticProps = async () => {
+	const aboutCms = new MicroCMS(API.ABOUT)
+	const about = await aboutCms.get()
+	return {
+		props: {
+			about
+		}
+	};
+};
---
+++ src/pages/works.tsx
@@ -0,0 +1,49 @@
+import { Component, ReactElement } from "react";
+import MicroCMS from "src/cms/microCms";
+import Header from "src/components/common/header";
+import Works from "src/components/works";
+import { API } from "src/constants/api";
+import { _About, _Works } from "src/types/cms";
+
+interface Props {
+	works: _Works,
+	about: _About
+}
+interface State { }
+
+export default class Index extends Component<Props, State> {
+
+	public state: State = {};
+	constructor(props: Props) {
+		super(props);
+		this.state = {};
+	}
+
+	public componentDidMount(): void {
+	}
+
+	public render(): ReactElement {
+		const {about, works} = this.props
+		return (
+			<>
+				<Header name={about.name}/>
+				<div className="top__wrapper">
+					<Works contents={works.contents} />
+				</div>
+			</>
+		);
+	}
+}
+
+export const getStaticProps = async () => {
+	const worksCms = new MicroCMS(API.WORKS)
+	const aboutCms = new MicroCMS(API.ABOUT)
+	const works = await (await worksCms.getList())
+	const about = await aboutCms.get()
+	return {
+		props: {
+			works,
+			about
+		}
+	};
+};
